1e9d95dcbd20e1189c26b7b38ee9ec4cb9f1c847
leo <l.hantsch@yahoo.fr>
2021-10-06 18:00:22 +0200

    Add of different text field on the profile page

--- nlpf_web_app/lib/ui/screens/profile.dart
+++ nlpf_web_app/lib/ui/screens/profile.dart
@@ -12,16 +12,79 @@ class _ProfileState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(backgroundColor: Colors.blueAccent),
-      body: Center(
-        child: FlatButton(
-          color: Colors.blueAccent,
-          textColor: Colors.white,
-          onPressed: () {
-            Navigator.pushNamed((context), '/');
-          },
-          child: Text('Profile'),
-        ),
-      ),
+      body:  Padding(  
+            padding: EdgeInsets.all(15),  
+            child: Column(  
+              children: <Widget>[  
+                Padding(  
+                  padding: EdgeInsets.all(15),  
+                  child: TextField(  
+                    decoration: InputDecoration(  
+                      border: OutlineInputBorder(),  
+                      labelText: 'Name',  
+                      hintText: 'Enter Your Name',  
+                    ),  
+                  ),  
+                ),  
+
+                Padding(  
+                  padding: EdgeInsets.all(15),  
+                  child: TextField(  
+                    decoration: InputDecoration(  
+                      border: OutlineInputBorder(),  
+                      labelText: 'Firstname',  
+                      hintText: 'Enter Your Firstname',  
+                    ),  
+                  ),  
+                ),  
+      
+                Padding(  
+                  padding: EdgeInsets.all(15),  
+                  child: TextField(  
+                    decoration: InputDecoration(  
+                      border: OutlineInputBorder(),  
+                      labelText: 'Offre 1',  
+                      hintText: 'Entrer la ville de votre première offre',  
+                    ),  
+                  ),  
+                ),  
+      
+
+                Padding(  
+                  padding: EdgeInsets.all(15),  
+                  child: TextField(  
+                    decoration: InputDecoration(  
+                      border: OutlineInputBorder(),  
+                      labelText: 'Offre 2',  
+                      hintText: 'Entrer la ville de votre deuxième offre',  
+                    ),  
+                  ),  
+                ),
+                
+                Padding(  
+                  padding: EdgeInsets.all(15),  
+                  child: TextField(  
+                    decoration: InputDecoration(  
+                      border: OutlineInputBorder(),  
+                      labelText: 'Offre 3',  
+                      hintText: 'Entrer le ville de votre troisième offre',  
+                    ),  
+                  ),  
+                ),  
+
+                Center(
+                  child: FlatButton(
+                    color: Colors.blueAccent,
+                    textColor: Colors.white,
+                    onPressed: () {
+                      Navigator.pushNamed((context), '/');
+                    },
+                    child: Text('Profile'),
+                  ),
+                )
+              ]
+            )
+      )
     );
   }
 }
