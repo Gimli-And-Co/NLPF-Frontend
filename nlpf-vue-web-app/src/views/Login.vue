43aa445eeb3e7ec938b7e3650c2b51e8e4d8be19
Martin Coulon <martin.coulon@epita.fr>
2021-10-13 13:14:19 +0200

    Add a login form

--- nlpf-vue-web-app/src/views/Login.vue
+++ nlpf-vue-web-app/src/views/Login.vue
@@ -1,10 +1,58 @@
 <template>
-  <div>Login</div>
+   <v-app >
+      <v-main>
+         <v-container fluid fill-height>
+            <v-layout align-center justify-center>
+               <v-flex xs12 sm8 md4>
+                  <v-card class="elevation-12">
+                     <v-toolbar dark color="primary">
+                        <v-toolbar-title>Connexion</v-toolbar-title>
+                     </v-toolbar>
+                     <v-card-text>
+                     <form ref="form" @submit.prevent="login()">
+                            <v-text-field
+                              v-model="mail"
+                              name="mail"
+                              label="Votre adresse mail"
+                              type="text"
+                              placeholder="mail"
+                              required
+                           ></v-text-field>
+                           
+                            <v-text-field
+                              v-model="password"
+                              name="password"
+                              label="Mot de passe"
+                              type="password"
+                              placeholder="password"
+                              required
+                           ></v-text-field>
+                           <v-btn type="submit" class="mt-4" color="primary" value="log in">Login</v-btn>
+                      </form>
+                     </v-card-text>
+                  </v-card>
+                
+               </v-flex>
+            </v-layout>
+         </v-container>
+      </v-main>
+   </v-app>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Login",
+  data() {
+    return {
+      mail: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      const { mail } = this;
+      console.log(mail + "s'est connecté")
+    },
+  },
+};
 </script>
-
-<style>
-</style>
